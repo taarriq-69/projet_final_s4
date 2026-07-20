43b081be31f6b185fc2c1360510baa11b3f584d9
Taarriq <blackcat.antsirabe@gmail.com>
2026-07-20 12:02:51 +0300

    #

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class TransactionModel extends Model
-}
+{
     protected $table            = 'transactions';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
